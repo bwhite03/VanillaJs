1c9c3ef20527af103e25a5290b5c4adadd9b8d2c
bwhite03 <bwhite1195@yahoo.com>
2022-02-21 10:19:23 -0500

    inital commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 3008,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Vanilla JavaScript',
+      template: 'public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js'],
+  },
+};
